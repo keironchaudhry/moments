e59d61a39554233bfde7ca33b7509d60b449fcc7
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-01-03 09:06:45 +0000

    Create sign in form task with handlechange, shandlesubmit and handleerror functions

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -8,14 +8,39 @@ import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import axios from "axios";
 
 function SignInForm() {
-    //   Add your component logic here
+
+    const [signInData, setSignInData] = useState({
+        username: '',
+        password: '',
+    });
+    const { username, password } = signInData;
+
+    const [errors, setErrors] = useState({});
+
+    const history = useHistory();
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value,
+        })
+    }
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post("/dj-rest-auth/login/", signInData);
+            history.push("/");
+        } catch (err) {
+            setErrors(err.response?.data);
+        }
+    }
 
     return (
         <Row className={styles.Row}>
@@ -25,15 +50,23 @@ function SignInForm() {
 
                     <h1 className={styles.Header}>sign in</h1>
 
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
+
                         <Form.Group controlId="username">
                             <Form.Label className="d-none">Username</Form.Label>
                             <Form.Control
                                 className={styles.Input}
                                 type="text"
                                 placeholder="Username"
-                                name="username" />
+                                name="username"
+                                value={username}
+                                onChange={handleChange} />
                         </Form.Group>
+                        {errors.username?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
 
                         <Form.Group controlId="password">
                             <Form.Label className="d-none">Password</Form.Label>
@@ -41,14 +74,26 @@ function SignInForm() {
                                 className={styles.Input}
                                 type="password"
                                 placeholder="Password"
-                                name="password" />
+                                name="password"
+                                value="password"
+                                onChange={handleChange} />
                         </Form.Group>
+                        {errors.password?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
 
                         <Button
                             className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
                             type="submit">
                             Sign in
                         </Button>
+                        {errors.non_field_errors?.map((message, idx) => (
+                            <Alert key={idx} variant="warning" className="mt-3">
+                                {message}
+                            </Alert>
+                        ))}
                     </Form>
 
                 </Container>
