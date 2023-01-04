cfc17406c3a5f2167cf2bf7d347937b29a28757b
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-01-04 11:49:06 +0000

    Fill out navbar component with logged in and logged out user icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,12 +4,53 @@ import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from 'react-router-dom';
 import { useCurrentUser } from '../contexts/CurrentUserContext';
+import Avatar from './Avatar';
 
 const NavBar = () => {
 
     const currentUser = useCurrentUser();
 
-    const loggedInIcons = <>{currentUser?.username}</>;
+    const addPostIcon = (
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to='/posts/create'>
+            <i className="far fa-plus-square"></i>Add post
+        </NavLink>
+    );
+
+    const loggedInIcons = (
+        <>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to='/feed'>
+                <i className="fas fa-stream"></i>Feed
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to='/liked'>
+                <i className="fas fa-heart"></i>Liked
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                to='/'
+                onClick={() => { }}>
+                <i className="fas fa-sign-out-alt"></i>Sign out
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                to={`/profiles/${currentUser?.profile_id}`}>
+                <Avatar
+                    src={currentUser?.profile_image}
+                    text='Profile'
+                    height={40}
+                />
+            </NavLink>
+        </>
+    );
+
     const loggedOutIcons = (
         <>
             <NavLink
@@ -35,6 +76,7 @@ const NavBar = () => {
                         <img src={logo} alt="logo" height="45px" />
                     </Navbar.Brand>
                 </NavLink>
+                {currentUser && addPostIcon}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
