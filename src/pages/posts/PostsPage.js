36b039f42e46edb6c6c37c72d949d46877e21446
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-01-09 17:24:24 +0000

    Refetch user posts on login and logout

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -18,12 +18,16 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
 
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
 function PostsPage({ message, filter = "" }) {
     const [posts, setPosts] = useState({ results: [] });
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
 
     const [query, setQuery] = useState("");
+
+    const currentUser = useCurrentUser();
 
     useEffect(() => {
         const fetchPosts = async () => {
@@ -44,7 +48,7 @@ function PostsPage({ message, filter = "" }) {
         return () => {
             clearTimeout(timer);
         };
-    }, [filter, query, pathname]);
+    }, [filter, query, pathname, currentUser]);
 
     return (
         <Row className="h-100">
